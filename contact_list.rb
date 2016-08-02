0a403e9e8f41ece695714215363bd95dbdb7f39e
vagrant <vagrant@lighthouse-labs>
2016-08-02 20:28:00 +0000

    Updated Contact List

--- contact_list.rb
+++ contact_list.rb
@@ -3,10 +3,40 @@ require_relative 'contact'
 # Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-  # TODO: Implement user interaction. This should be the only file where you use `puts` and `gets`.
+  def initialize
+
+    #menu
+    case ARGV[0]
+    when "list"
+      puts Contact.all
+    when "show"
+      puts Contact.find(ARGV[1].to_i)
+    when "search"
+      puts Contact.search(ARGV[1])
+    when "new"
+      puts "Please enter the full name of the new contact"
+      name = STDIN.gets.strip 
+      puts "Please enter the contact's e-mail address"
+      email = STDIN.gets.strip
+      puts Contact.create(name, email)
+    else
+      menu
+    end
+  end
+
+  def menu
+    puts "Here is a list of available commands:"
+    puts ""
+    puts "new    - Create a new contact"
+    puts "list   - List all contacts"
+    puts "show   - Show an individual contact"
+    puts "search - Search contacts"
+  end  
 
 end
 
 # p Contact.create("Jeremy", "legendinhisownmind@gmail.com")
 # p Contact.find(2)
-p Contact.search("imon")
+# p Contact.search("imon")
+
+ContactList.new
